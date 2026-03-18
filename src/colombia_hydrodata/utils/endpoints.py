53efd008e6736400b0b2cdf26f31f803e1aba712
Sebástian Narváez <sebnarvaez19@outlook.com>
2026-03-17 20:26:52 -0500

    feat: data endpoints

---
+++ src/colombia_hydrodata/utils/endpoints.py
@@ -0,0 +1,3 @@
+datos_gov_co_cne = "https://www.datos.gov.co/resource/hp9r-jxuu.json"
+aquarius_webportal = "http://aquariuswebportal.ideam.gov.co/"
+aquarius_webportal_datasets = "http://aquariuswebportal.ideam.gov.co/Data/DatasetGrid"
