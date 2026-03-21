26ea64acbe35fcc6c1e39086c3b758fa0cd0d4f2
Sebástian Narváez <sebnarvaez19@outlook.com>
2026-03-20 22:37:15 -0500

    refactor: save attributes in an specific module

---
+++ src/colombia_hydrodata/attributes.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from typing import Self
+
+from colombia_hydrodata.utils.fetch.stations import station_hydrographic_data, station_location_data
+
+
+@dataclass(frozen=True)
+class Location:
+    altitude: float
+    longitude: float
+    latitude: float
+
+    def __str__(self) -> str:
+        return f"Location: altitude={self.altitude:0.2f} [{self.longitude:0.3f}; {self.latitude:0.3f}]"
+
+    @classmethod
+    def from_stations_df(cls, station_id: str) -> Self:
+        ld = station_location_data(station_id)
+        return cls(altitude=ld["altitude"], longitude=ld["longitude"], latitude=ld["latitude"])
+
+
+@dataclass(frozen=True)
+class Hydrographic:
+    hydrographic_area: str
+    hydrographic_zone: str
+    hydrigraphic_subzone: str
+
+    def __str__(self) -> str:
+        return f"Hydrographic: area={self.hydrographic_area} zone={self.hydrographic_zone} subzone={self.hydrigraphic_subzone}"
+
+    @classmethod
+    def from_stations_df(cls, station_id: str) -> Self:
+        hd = station_hydrographic_data(station_id)
+        return cls(hydrographic_area=hd["hydrographic_area"], hydrographic_zone=hd["hydrographic_zone"], hydrigraphic_subzone=hd["hydrographic_subzone"])
+
+
+@dataclass(frozen=True)
+class Variable:
+    param: str
+    label: str
+    id: int
+
+    def __str__(self) -> str:
+        return f"{self.param}@{self.label}".upper()
+
+    def fetch_data(self) -> str:
+        return f"{str(self)}: ({self.id=})"  # TODO: Fetch dataset logic
--- src/colombia_hydrodata/station.py
+++ src/colombia_hydrodata/station.py
@@ -2,45 +2,11 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Self
 
+import pandas as pd
+
+from colombia_hydrodata.attributes import Hydrographic, Location, Variable
 from colombia_hydrodata.utils.fetch.aquarius import station_datasets
 from colombia_hydrodata.utils.fetch.stations import station_data, station_hydrographic_data, station_location_data
-
-
-@dataclass(frozen=True)
-class StationLocation:
-    altitude: float
-    longitude: float
-    latitude: float
-
-    @classmethod
-    def from_stations_df(cls, station_id: str) -> Self:
-        ld = station_location_data(station_id)
-        return cls(altitude=ld["altitude"], longitude=ld["longitude"], latitude=ld["latitude"])
-
-
-@dataclass(frozen=True)
-class StationHydrographic:
-    hydrographic_area: str
-    hydrographic_zone: str
-    hydrigraphic_subzone: str
-
-    @classmethod
-    def from_stations_df(cls, station_id: str) -> Self:
-        hd = station_hydrographic_data(station_id)
-        return cls(hydrographic_area=hd["hydrographic_area"], hydrographic_zone=hd["hydrographic_zone"], hydrigraphic_subzone=hd["hydrographic_subzone"])
-
-
-@dataclass(frozen=True)
-class Variable:
-    param: str
-    label: str
-    id: int
-
-    def __str__(self) -> str:
-        return f"{self.param}@{self.label}".upper()
-
-    def fetch_data(self) -> str:
-        return f"{str(self): (self.id=)}"  # TODO: Fetch dataset logic
 
 
 @dataclass(frozen=True)
@@ -55,8 +21,8 @@ class Station:
     installation_date: datetime | None
     suspension_date: datetime | None
     owner: str
-    location: StationLocation
-    hydrographic: StationHydrographic
+    location: Location
+    hydrographic: Hydrographic
     variables: dict[str, Variable] | None = None
 
     @classmethod
@@ -77,10 +43,8 @@ class Station:
             installation_date=sd["installation_date"],
             suspension_date=sd["suspension_date"],
             owner=sd["owner"],
-            location=StationLocation(altitude=ld["altitude"], longitude=ld["longitude"], latitude=ld["latitude"]),
-            hydrographic=StationHydrographic(
-                hydrographic_area=hd["hydrographic_area"], hydrographic_zone=hd["hydrographic_zone"], hydrigraphic_subzone=hd["hydrographic_subzone"]
-            ),
+            location=Location(ld["altitude"], ld["longitude"], ld["latitude"]),
+            hydrographic=Hydrographic(hd["hydrographic_area"], hd["hydrographic_zone"], hd["hydrographic_subzone"]),
             variables=vars,
         )
 
